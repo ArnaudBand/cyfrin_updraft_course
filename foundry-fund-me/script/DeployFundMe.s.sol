22bff27cb06a5aae8254900ee6de6ca295eb236e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-20 00:47:33 +0300

    Deploy the HelperConfig smart contract before the startbroadcast to get the active network

--- foundry-fund-me/script/DeployFundMe.s.sol
+++ foundry-fund-me/script/DeployFundMe.s.sol
@@ -3,11 +3,17 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe) {
+//        Before startbroadcast -> Not a "real" tx
+        HelperConfig helpConfig = new HelperConfig();
+        address ethUsdPriceFeed = helpConfig.activeNetworkConfig();
+// After startbroadcast -> Real tx
         vm.startBroadcast();
-        new FundMe();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
