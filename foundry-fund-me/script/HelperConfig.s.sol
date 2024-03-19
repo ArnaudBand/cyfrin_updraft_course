68cff98edf53dee3022988c4fdd402f01840de80
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-20 00:44:19 +0300

    Create a heplerConfig script to config different networks

---
+++ foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,55 @@
+//1. Deploy mocks when we are on a local anvil chain
+//2. Keep track of contract address across
+//Sepolia ETH/USD
+//Mainnet ETH/USD
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggreagator.sol";
+
+contract HelperConfig is Script {
+    /* if we are on a local anvil, we deploy mocks
+    Otherwise, grab the existing address from the live network */
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if(block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        }
+        else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory selopiaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return selopiaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns(NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
+        return mainnetConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return anvilConfig;
+    }
+}
