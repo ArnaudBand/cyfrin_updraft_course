6bc44ad909c1db851ebd2673f40bdda077459a6f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-20 23:49:18 +0300

    Add interactions script

---
+++ foundry-fund-me/script/Interactions.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/src/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {Script, console} from "forge-std/src/Script.sol";
+import { FundMe } from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 public constant SEND_VALUE = 0.1 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+        fundFundMe(mostRecentDeployed);
+    }
+}
+
+contract WithdrawFundMe is Script {
+
+}
