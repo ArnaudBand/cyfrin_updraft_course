681672ff4768d8d236d0eec00d234bb5903ba45a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-22 19:13:51 +0300

    Deploy Raffle smart contract

--- foundry-smart-contract-lottery/script/DeployRaffle.s.sol
+++ foundry-smart-contract-lottery/script/DeployRaffle.s.sol
@@ -3,7 +3,23 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit);
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
 }
