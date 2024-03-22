7e892fda28526102302326221a3334a5ef9c1a9f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-22 18:04:04 +0300

    Create a script and HelperConfig to deploy on many networks

---
+++ foundry-smart-contract-lottery/script/DeployRaffle.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {}
+}
---
+++ foundry-smart-contract-lottery/script/HelperConfig.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 1115511) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            entranceFee: 0.01 ether,
+            interval: 30,
+            vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+            gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+            subscriptionId: 0, // Update this our subscriptionId!
+            callbackGasLimit: 500000 // gas!!!
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig) {
+        if(activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+    }
+}
