85a0cc01a51b394e96ed21fb7a50af08eda9e2fd
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-26 12:28:23 +0300

    Add a consumer contract and fund

--- foundry-smart-contract-lottery/script/DeployRaffle.s.sol
+++ foundry-smart-contract-lottery/script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
-import { CreateSubscription, FundSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -22,13 +22,18 @@ contract DeployRaffle is Script {
         if (subscriptionId == 0) {
             CreateSubscription createSubscription = new CreateSubscription();
             subscriptionId = createSubscription.createSubscription(vrfCoordinator);
+
+            FundSubscription fundSub = new FundSubscription();
+            fundSub.fundSubscription(
+            vrfCoordinator, subscriptionId, link
+            );
         }
 
         vm.startBroadcast();
-        Raffle raffle = new Raffle(
-            entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit
-        );
+        Raffle raffle = new Raffle(entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit);
         vm.stopBroadcast();
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(address(raffle), vrfCoordinator, subscriptionId);
         return (raffle, helperConfig);
     }
 }
