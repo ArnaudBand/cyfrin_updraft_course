6acaa2004f78d12d7e96e357ceb966a7a5b8500e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-07 02:49:46 +0300

    Add the Address of the contract

--- foundry-erc20/script/DeployOurToken.s.sol
+++ foundry-erc20/script/DeployOurToken.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {Script} from 'forge-std/Script.sol';
 import {OurToken} from "../src/OurToken.sol";
 
+// Address of the ERC20: 0x71E6d438237762a6d3099D9cAcEb6c10582a4cd9
+
 contract DeployOurToken is Script {
     uint256 public constant INITIAL_SUPPLY = 100 ether;
     function run() external {
