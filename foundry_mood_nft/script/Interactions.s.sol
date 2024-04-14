a9811b33f6c14e3dcc90cce8c0afcbd182f217f2
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-14 21:19:20 +0000

    Update mostRecentDeployed variable in Interactions.s.sol

--- foundry_mood_nft/script/Interactions.s.sol
+++ foundry_mood_nft/script/Interactions.s.sol
@@ -10,7 +10,7 @@ contract MintDogeNFT is Script {
         string public constant PUG =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     function run() external {
-        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("DogeNFT", block.chainid);
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("DOGIE", block.chainid);
         mintNFtOncontract(mostRecentDeployed);
     }
 
