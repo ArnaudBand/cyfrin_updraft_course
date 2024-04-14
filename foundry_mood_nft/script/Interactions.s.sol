182b32c5c6526aea6d16480b08aae9013bf1b517
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-14 11:20:51 +0000

    Fix import paths in Interactions.s.sol

--- foundry_mood_nft/script/Interactions.s.sol
+++ foundry_mood_nft/script/Interactions.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
-import {DevOpsTools} from "lib/foundry-devops/DevOpsTools.sol";
-import {DogeNFT} from "../src/Intractions.s.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {DogeNFT} from "../src/DogeNFT.sol";
 
 contract MintDogeNFT is Script {
 
         string public constant PUG =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     function run() external {
-        address mostRecentDeployed = DevOpsTools.get_most_recent_deployed("DogeNFT", block.chainid);
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("DogeNFT", block.chainid);
         mintNFtOncontract(mostRecentDeployed);
     }
 
-    function mintNFtOncontract(address contractAddress) internal {
-        vm.startbroadcast();
+    function mintNFtOncontract(address contractAddress) public {
+        vm.startBroadcast();
         DogeNFT(contractAddress).mintNFT(PUG);
-        vm.stopbroadcast();
+        vm.stopBroadcast();
     }
 }
