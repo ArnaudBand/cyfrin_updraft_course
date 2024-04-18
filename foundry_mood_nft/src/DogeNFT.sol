06a075389b1391ea4b90ef238d99d9fc59984fbf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-18 14:20:40 +0000

    Refactor DogeNFT contract to use tokenUri instead of tokenURI

--- foundry_mood_nft/src/DogeNFT.sol
+++ foundry_mood_nft/src/DogeNFT.sol
@@ -4,21 +4,29 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DogeNFT is ERC721 {
-    uint256 private s_tokenCounter;
+    error DogeNFT__TokenUriNotFound();
 
-    mapping(uint256 => string) private s_tokenURIs;
+    mapping(uint256 tokenId => string tokenUri) private s_tokenURIs;
+    uint256 private s_tokenCounter;
 
     constructor() ERC721("DOGIE", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNFT(string memory tokenURI) public {
-        s_tokenURIs[s_tokenCounter] = tokenURI;
+    function mintNFT(string memory tokenUri) public {
+        s_tokenURIs[s_tokenCounter] = tokenUri;
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert DogeNFT__TokenUriNotFound();
+        }
         return s_tokenURIs[tokenId];
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
     }
 }
