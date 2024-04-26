38e431d96871c074b65fb990fa3b5ab8a8024eec
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-04-26 21:13:09 +0000

    Update DeployMoodNFT contract to include SVG image URIs

--- foundry_mood_nft/script/DeployMoodNFT.s.sol
+++ foundry_mood_nft/script/DeployMoodNFT.s.sol
@@ -1,16 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Script} from '../lib/forge-std/src/Script.sol';
-import {MoodNFT} from '../src//MoodNFT.sol';
+import {Script, console} from '../../lib/forge-std/src/Script.sol';
+import {MoodNFT} from '../../src//MoodNFT.sol';
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNFT is Script {
     function run() external returns(MoodNFT) {
-        
+        string memory sadSVG = vm.readFile("./images/sad.svg");
+        string memory happySVG = vm.readFile("./images/happy.svg");
+        MoodNFT moodNFT = new MoodNFT(
+            svgToImageURI(sadSVG),
+            svgToImageURI(happySVG)
+        );
+        return moodNFT;
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
-        return string(abi.encodePacked('data:image/svg+xml;base64,', Base64.encode(bytes(svg))));
+        string memory baseUrl = "data:image/svg+xml;base64,";
+        string memory svgBase64Encode = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+        return string(abi.encodePacked(baseUrl, svgBase64Encode));
     }
 }
