291afe34168d050895f8d89c08126387ca221e21
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-09 16:37:18 +0300

    Refactor DeployMoodNFT contract to use consistent import statements

--- foundry_mood_nft/script/DeployMoodNFT.s.sol
+++ foundry_mood_nft/script/DeployMoodNFT.s.sol
@@ -1,26 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Script, console} from 'forge-std/Script.sol';
-import {MoodNFT} from '../src/MoodNFT.sol';
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNFT} from "../src/MoodNFT.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNFT is Script {
-    function run() external returns(MoodNFT) {
+    function run() external returns (MoodNFT) {
         string memory sadSVG = vm.readFile("./images/sad.svg");
         string memory happySVG = vm.readFile("./images/happy.svg");
-        MoodNFT moodNFT = new MoodNFT(
-            svgToImageURI(sadSVG),
-            svgToImageURI(happySVG)
-        );
+        MoodNFT moodNFT = new MoodNFT(svgToImageURI(sadSVG), svgToImageURI(happySVG));
         return moodNFT;
     }
 
     function svgToImageURI(string memory svg) public pure returns (string memory) {
         string memory baseUrl = "data:image/svg+xml;base64,";
-        string memory svgBase64Encode = Base64.encode(
-            bytes(string(abi.encodePacked(svg)))
-        );
+        string memory svgBase64Encode = Base64.encode(bytes(string(abi.encodePacked(svg))));
         return string(abi.encodePacked(baseUrl, svgBase64Encode));
     }
 }
