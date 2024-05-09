8f606a0cc9088b05ba25546ea72ce1c715d5f910
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-09 16:39:27 +0300

    Update solidity version to 0.8.24 in MoodNFTTest.t.sol

--- foundry_mood_nft/test/unit/MoodNFTTest.t.sol
+++ foundry_mood_nft/test/unit/MoodNFTTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MoodNFT} from "../../src/MoodNFT.sol";
@@ -14,6 +14,7 @@ contract MoodNFTTest is Test {
         "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEwMCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8+CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNjEiIGN5PSI4MiIgcj0iMTIiLz4KICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==";
 
     address USER = makeAddr("user");
+
     function setUp() public {
         moodNFT = new MoodNFT(SAD_SVG_URI, HAPPY_SVG_URI);
     }
