6cdcfd54c9796f0241db54f0feff683a11f45fbd
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-17 02:46:42 +0300

    feat: Add DecentralizedStableCoin contract

---
+++ foundry_defi_protocol/src/DecentralizedStableCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Decentralized Stable Coin
+ * @author Arnaud
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DDSCEngine. This contract is the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin {}
