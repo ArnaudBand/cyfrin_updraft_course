16c9e30f6d9d82b72a9dc1d4dc4d9464775bf3bb
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-17 02:48:33 +0300

    feat: Add ERC20Burnable and Ownable to DecentralizedStableCoin contract

--- foundry_defi_protocol/src/DecentralizedStableCoin.sol
+++ foundry_defi_protocol/src/DecentralizedStableCoin.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /**
  * @title Decentralized Stable Coin
  * @author Arnaud
@@ -10,4 +13,17 @@ pragma solidity ^0.8.19;
  *
  * This is the contract meant to be governed by DDSCEngine. This contract is the ERC20 implementation of our stablecoin system.
  */
-contract DecentralizedStableCoin {}
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+
+  error DecentralizedStableCoin__MustBeMoreThanZero();
+  error DecentralizedStableCoin__BurnAmountExceedsBalance();
+  
+  constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+  function burn(uint256 _amount) public override onlyOwner {
+    uint256 balance = balanceOf(msg.sender);
+    if (_amount <= 0) revert DecentralizedStableCoin__MustBeMoreThanZero();
+    if (balance < _amount) revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+    super.burn(_amount);
+  }
+}
