ebff63c6be2808f46a9f6f1cfb96f65eec804de6
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-17 08:44:04 +0300

    feat: Implement core DSCEngine contract for the Decentralized Stablecoin system

--- foundry_defi_protocol/src/DSCEngine.sol
+++ foundry_defi_protocol/src/DSCEngine.sol
@@ -1,4 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+/**
+ * @title DSC Engine
+ * @author Arnaud
+ * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == 1$ peg.
+ * This stablecoin has properties:
+ * - Exogenous Collateral
+ * - Dollar Pegged
+ * - Algorithmically Stable
+ *
+ * It is similar to DAI if DAI had no gorvernance, no fees, and was only backed by WETH and WBTC.
+ * Our DSC system should always be "overcollateralized". At no point, should the value of
+ * all collateral < the $ backed value of all the DSC.
+ *
+ * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
+ * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
+ * @notice This contract is based on the MakerDAO DSS system
+ */
 contract DSCEngine {}
