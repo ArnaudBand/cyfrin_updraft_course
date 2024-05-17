40eaf55e140ae5be43b28179297cc871c2c1fb08
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-17 09:46:33 +0300

    feat: Add error handling and modifiers to DSCEngine contract

--- foundry_defi_protocol/src/DSCEngine.sol
+++ foundry_defi_protocol/src/DSCEngine.sol
@@ -1,6 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+
 /**
  * @title DSC Engine
  * @author Arnaud
@@ -18,4 +20,50 @@ pragma solidity ^0.8.19;
  * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
  * @notice This contract is based on the MakerDAO DSS system
  */
-contract DSCEngine {}
+contract DSCEngine {
+    // errors
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine_NotAllowedToken();
+
+    // State variables
+    mapping(address token => address priceFeed) private s_PriceFeeds;
+
+    // Immutables
+    DecentralizedStableCoin private immutable i_dsc;
+
+    // modifier
+    modifier moreThanZero(uint256 _amount) {
+        if (_amount == 0) revert DSCEngine__NeedsMoreThanZero();
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_PriceFeeds[token] == address(0)) revert DSCEngine_NotAllowedToken();
+        _;
+    }
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+          s_PriceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    /*
+     * @notice Deposit collateral into the system
+     * @param tokenCollateralAddress The address of the collateral token
+     * @param _amount The amount of collateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 _amountCollateral)
+        external
+        moreThanZero(_amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        
+    {}
+}
