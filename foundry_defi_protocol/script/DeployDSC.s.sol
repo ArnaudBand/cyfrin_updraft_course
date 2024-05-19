da3fb5436a78f88bff27326f4a0d54b9bcf5f824
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-19 19:52:30 +0300

    feat: Add DeployDSC script for deploying DecentralizedStableCoin and DSCEngine contracts

---
+++ foundry_defi_protocol/script/DeployDSC.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+
+contract DeployDSC is Script {
+
+  address[] public tokenAddresses;
+  address[] public priceFeedAddresses;
+  function run() external returns(DecentralizedStableCoin, DSCEngine) {
+    HelperConfig helperConfig = new HelperConfig();
+    (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = helperConfig.activeNetworkConfig();
+    tokenAddresses = [weth, wbtc];
+    priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+    vm.startBroadcast();
+    DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+    DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+    dsc.transferOwnership(address(engine));
+    vm.stopBroadcast();
+    return (dsc, engine);
+  }
+}
