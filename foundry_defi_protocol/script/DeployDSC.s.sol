8660d9146180f2602a9d21008e35bf030972327c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-06 10:53:15 +0000

    feat: Update DeployDSC script to include HelperConfig in return statement

--- foundry_defi_protocol/script/DeployDSC.s.sol
+++ foundry_defi_protocol/script/DeployDSC.s.sol
@@ -7,19 +7,20 @@ import {DSCEngine} from "../src/DSCEngine.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
-  address[] public tokenAddresses;
-  address[] public priceFeedAddresses;
-  function run() external returns(DecentralizedStableCoin, DSCEngine) {
-    HelperConfig helperConfig = new HelperConfig();
-    (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = helperConfig.activeNetworkConfig();
-    tokenAddresses = [weth, wbtc];
-    priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
-    vm.startBroadcast();
-    DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-    DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
-    dsc.transferOwnership(address(engine));
-    vm.stopBroadcast();
-    return (dsc, engine);
-  }
+    function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+        return (dsc, engine, helperConfig);
+    }
 }
