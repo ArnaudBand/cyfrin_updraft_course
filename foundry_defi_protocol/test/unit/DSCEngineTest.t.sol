3f072aa53a575234b2a9cf64494605fb3f7cd5f7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-06 10:54:59 +0000

    test: Add unit test for DSCEngine.getUsdValue function

---
+++ foundry_defi_protocol/test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+
+contract DSCEngineTest is Test {
+    DecentralizedStableCoin dsc;
+    DSCEngine engine;
+    HelperConfig config;
+
+    address public ethUsdPriceFeed;
+    address public btcUsdPriceFeed;
+    address public weth;
+    address public wbtc;
+    uint256 public deployerKey;
+
+    function setUp() public {
+        DeployDSC deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (ethUsdPriceFeed, , weth, , deployerKey) = config.activeNetworkConfig();
+    }
+
+    function testGetUsdValue() public view {
+        uint256 ethAmount = 15e18;
+        // 15e18 ETH * $2000/ETH = $30,000e18
+        uint256 expectedUsd = 30000e18;
+        uint256 usdValue = engine.getUsdValue(weth, ethAmount);
+        console.log(usdValue);
+        assertEq(usdValue, expectedUsd);
+    }
+}
