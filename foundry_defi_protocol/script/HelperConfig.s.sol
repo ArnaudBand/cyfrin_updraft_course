10c3fec1ddd0f0ec5cdcfac5d8002b971aaba7df
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-06 10:53:02 +0000

    feat: Refactor HelperConfig contract for improved readability and maintainability

--- foundry_defi_protocol/script/HelperConfig.s.sol
+++ foundry_defi_protocol/script/HelperConfig.s.sol
@@ -6,65 +6,59 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {ERC20Mock} from "../test/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
-  struct NetworkConfig {
-    address wethUsdPriceFeed;
-    address wbtcUsdPriceFeed;
-    address weth;
-    address wbtc;
-    uint256 deployerKey;
-  }
-
-  uint8 public constant DECIMALS = 8;
-  int256 public constant ETH_USD_PRICE = 2000e8;
-  int256 public constant WBTC_USD_PRICE = 40000e8;
-  uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-
-  NetworkConfig public activeNetworkConfig;
-
-  constructor() {
-    if (block.chainid == 11155111) {
-      activeNetworkConfig = getSepoliaEthConfig();
-    } else {
-      activeNetworkConfig = getOrCreateAnvilEthConfig();
-    }
-  }
-
-  function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
-    return NetworkConfig ({
-      wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
-      wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
-      weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
-      wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
-      deployerKey: vm.envUint("PRIVATE_KEY")
-    });
-  }
-
-  function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
-    if (activeNetworkConfig.wethUsdPriceFeed == address(0)) {
-      return activeNetworkConfig;
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+        address weth;
+        address wbtc;
+        uint256 deployerKey;
     }
 
-    vm.startBroadcast();
-    MockV3Aggregator ethUSDPriceFeed = new MockV3Aggregator(
-      DECIMALS,
-      ETH_USD_PRICE
-    );
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant WBTC_USD_PRICE = 40000e8;
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
-    ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+    NetworkConfig public activeNetworkConfig;
 
-    MockV3Aggregator wbtcUSDPriceFeed = new MockV3Aggregator(
-      DECIMALS,
-      WBTC_USD_PRICE
-    );
-    ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
-    vm.stopBroadcast();
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
 
-    return NetworkConfig({
-      wethUsdPriceFeed: address(ethUSDPriceFeed),
-      wbtcUsdPriceFeed: address(wbtcUSDPriceFeed),
-      weth: address(wethMock),
-      wbtc: address(wbtcMock),
-      deployerKey: vm.envUint("PRIVATE_KEY")
-    });
-  }
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
+            wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
+            wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
+            weth: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
+            wbtc: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
+            deployerKey: vm.envUint("PRIVATE_KEY")
+        });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator ethUSDPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+
+        MockV3Aggregator wbtcUSDPriceFeed = new MockV3Aggregator(DECIMALS, WBTC_USD_PRICE);
+        ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            wethUsdPriceFeed: address(ethUSDPriceFeed),
+            wbtcUsdPriceFeed: address(wbtcUSDPriceFeed),
+            weth: address(wethMock),
+            wbtc: address(wbtcMock),
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
+        });
+    }
 }
