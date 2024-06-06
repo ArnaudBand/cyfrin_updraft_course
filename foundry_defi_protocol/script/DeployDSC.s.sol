cac1fdb2c2f5e04c754b228061c433ea521cc12c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-06 11:03:36 +0000

    feat: Update DeployDSC script to start broadcasting with deployer key

--- foundry_defi_protocol/script/DeployDSC.s.sol
+++ foundry_defi_protocol/script/DeployDSC.s.sol
@@ -16,7 +16,7 @@ contract DeployDSC is Script {
             helperConfig.activeNetworkConfig();
         tokenAddresses = [weth, wbtc];
         priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
-        vm.startBroadcast();
+        vm.startBroadcast(deployerKey);
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
         DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
         dsc.transferOwnership(address(engine));
