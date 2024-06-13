102edb8c568b6498f3a5e2f9bdb7d06ec9102dd2
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-13 04:57:56 +0000

    refactor: Add additional getter functions to DSCEngine.sol

--- foundry_defi_protocol/src/DSCEngine.sol
+++ foundry_defi_protocol/src/DSCEngine.sol
@@ -257,6 +257,38 @@ contract DSCEngine is ReentrancyGuard {
         return PRECISION;
     }
 
+    function getAdditionalFeedPrecision() external pure returns (uint256) {
+        return ADDITIONAL_FEED_PRECISION;
+    }
+
+    function getLiquidationThreshold() external pure returns (uint256) {
+        return LIQUIDATION_THRESHOLD;
+    }
+
+        function getLiquidationBonus() external pure returns (uint256) {
+        return LIQUIDATION_BONUS;
+    }
+
+    function getLiquidationPrecision() external pure returns (uint256) {
+        return LIQUIDATION_PRECISION;
+    }
+
+    function getMinHealthFactor() external pure returns (uint256) {
+        return MIN_HEALTH_FACTOR;
+    }
+
+    function getCollateralTokens() external view returns (address[] memory) {
+        return s_collateralTokens;
+    }
+
+    function getDsc() external view returns (address) {
+        return address(i_dsc);
+    }
+
+    function getCollateralTokenPriceFeed(address token) external view returns (address) {
+        return s_PriceFeeds[token];
+    }
+
     // Private & Internal view functions
 
         /**
