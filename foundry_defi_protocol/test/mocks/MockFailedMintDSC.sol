4f7e0a35ce2f398e7901827c9bfec6c6f054bcaf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-14 09:33:08 +0000

    refactor: Fix import formatting in MockFailedMintDSC.sol

--- foundry_defi_protocol/test/mocks/MockFailedMintDSC.sol
+++ foundry_defi_protocol/test/mocks/MockFailedMintDSC.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MockFailedMintDSC is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__AmountMustBeMoreThanZero();
@@ -17,7 +17,7 @@ contract MockFailedMintDSC is ERC20Burnable, Ownable {
     Related code changes can be viewed in this commit:
     https://github.com/OpenZeppelin/openzeppelin-contracts/commit/13d5e0466a9855e9305119ed383e54fc913fdc60
     */
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0x9C8A2750E8814eDF52224a92bD61B4F596a94c17) { }
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(0x9C8A2750E8814eDF52224a92bD61B4F596a94c17) {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
