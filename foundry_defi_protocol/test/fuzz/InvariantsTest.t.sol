4805d740ae1bfcd65de5955c5f2d754b9131a49a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-27 11:35:58 +0000

    refactor: Add InvariantsTest contract for testing protocol invariants

---
+++ foundry_defi_protocol/test/fuzz/InvariantsTest.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {Handler} from "./Handler.t.sol";
+
+contract InvariantsTest is StdInvariant, Test {
+
+    DSCEngine engine;
+    DecentralizedStableCoin dsc;
+    DeployDSC deployer;
+    HelperConfig config;
+    Handler handler;
+
+    address weth;
+    address wbtc;
+
+    function setUp() external {
+        deployer = new DeployDSC();
+        (dsc, engine, config) = deployer.run();
+        (, , weth, wbtc, ) = config.activeNetworkConfig();
+        handler = new Handler(engine, dsc);
+        targetContract(address(handler));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+      uint256 totalSupply = dsc.totalSupply();
+      uint256 totalWethDeposited = ERC20Mock(weth).balanceOf(address(engine));
+      uint256 totalWbtcDeposited = ERC20Mock(wbtc).balanceOf(address(engine));
+
+      uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+      uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+
+      console.log("wethValue: %d", wethValue);
+      console.log("wbtcValue: %d", wbtcValue);
+      console.log("totalSupply: %d", totalSupply);
+
+      assert(wethValue + wbtcValue >= totalSupply);
+    }
+}
