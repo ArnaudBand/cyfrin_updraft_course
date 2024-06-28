a1bb9736758b92798632c62ab5c211686cbf32a3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-28 05:01:37 +0000

    refactor: Improve Handler contract by renaming depositCollateral function to mintAndDepositCollateral and adding additional checks in redeemCollateral function

--- foundry_defi_protocol/test/fuzz/Handler.t.sol
+++ foundry_defi_protocol/test/fuzz/Handler.t.sol
@@ -7,45 +7,44 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract Handler is Test {
+    DSCEngine engine;
+    DecentralizedStableCoin dsc;
 
-  DSCEngine engine;
-  DecentralizedStableCoin dsc;
+    ERC20Mock weth;
+    ERC20Mock wbtc;
 
-  ERC20Mock weth;
-  ERC20Mock wbtc;
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max; // The max uint96 value
 
-  uint256 MAX_DEPOSIT_SIZE = type(uint96).max; // The max uint96 value
+    constructor(DSCEngine _engine, DecentralizedStableCoin _dsc) {
+        engine = _engine;
+        dsc = _dsc;
 
-  constructor(DSCEngine _engine, DecentralizedStableCoin _dsc) {
-    engine = _engine;
-    dsc = _dsc;
+        address[] memory collateralTokens = engine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
 
-    address[] memory collateralTokens = engine.getCollateralTokens();
-    weth = ERC20Mock(collateralTokens[0]);
-    wbtc = ERC20Mock(collateralTokens[1]);
-  }
+    // redeem collateral <-
+    function mintAndDepositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(engine), amountCollateral);
+        engine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
 
-  // redeem collateral <-
-  function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
-    amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
-    ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
-    vm.startPrank(msg.sender);
-    collateral.mint(msg.sender, amountCollateral);
-    collateral.approve(address(engine), amountCollateral);
-    engine.depositCollateral(address(collateral), amountCollateral);
-    vm.stopPrank();
-  }
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateralToRedeem = engine.getCollateralBalanceOfUser(msg.sender, address(collateral));
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if (amountCollateral == 0) return;
+        vm.prank(msg.sender);
+        engine.redeemCollateral(address(collateral), amountCollateral);
+    }
 
-  function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
-    ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
-    uint256 maxCollateral = engine.getCollateralBalanceOfUser(msg.sender, address(collateral));
-    amountCollateral = bound(amountCollateral, 1, maxCollateral);
-    if (amountCollateral == 0) return;
-
-    engine.redeemCollateral(address(collateral), amountCollateral);
-  }
-
-  function _getCollateralFromSeed(uint256 seed) private view returns(ERC20Mock) {
-    return seed % 2 == 0 ? weth : wbtc;
-  }
+    function _getCollateralFromSeed(uint256 seed) private view returns (ERC20Mock) {
+        return seed % 2 == 0 ? weth : wbtc;
+    }
 }
