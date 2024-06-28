b8d081f26c662990af9fba6bef95170b7151c74b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-28 05:01:15 +0000

    refactor: Update InvariantsTest contract to use IERC20 interface for collateral balance retrieval

--- foundry_defi_protocol/test/fuzz/InvariantsTest.t.sol
+++ foundry_defi_protocol/test/fuzz/InvariantsTest.t.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-import {ERC20Mock} from "../mocks/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
@@ -11,7 +12,6 @@ import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {Handler} from "./Handler.t.sol";
 
 contract InvariantsTest is StdInvariant, Test {
-
     DSCEngine engine;
     DecentralizedStableCoin dsc;
     DeployDSC deployer;
@@ -24,23 +24,25 @@ contract InvariantsTest is StdInvariant, Test {
     function setUp() external {
         deployer = new DeployDSC();
         (dsc, engine, config) = deployer.run();
-        (, , weth, wbtc, ) = config.activeNetworkConfig();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
         handler = new Handler(engine, dsc);
         targetContract(address(handler));
     }
 
     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
-      uint256 totalSupply = dsc.totalSupply();
-      uint256 totalWethDeposited = ERC20Mock(weth).balanceOf(address(engine));
-      uint256 totalWbtcDeposited = ERC20Mock(wbtc).balanceOf(address(engine));
+        // get the value of the all the collateral in the protocol
+        // compare it to all the debt(dsc)
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(engine));
+        uint256 totalWBtcDeposited = IERC20(wbtc).balanceOf(address(engine));
 
-      uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
-      uint256 wbtcValue = engine.getUsdValue(wbtc, totalWbtcDeposited);
+        uint256 wethValue = engine.getUsdValue(weth, totalWethDeposited);
+        uint256 wbtcValue = engine.getUsdValue(wbtc, totalWBtcDeposited);
 
-      console.log("wethValue: %d", wethValue);
-      console.log("wbtcValue: %d", wbtcValue);
-      console.log("totalSupply: %d", totalSupply);
+        console.log("weth value: ", wethValue);
+        console.log("wbtc value: ", wbtcValue);
+        console.log("Total Supply: ", totalSupply);
 
-      assert(wethValue + wbtcValue >= totalSupply);
+        assert(wethValue + wbtcValue >= totalSupply);
     }
 }
