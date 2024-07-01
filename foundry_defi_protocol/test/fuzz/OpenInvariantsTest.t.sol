31cea3d8c247ec629be42a4c219ecb40a73848f9
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-01 06:38:27 +0000

    refactor: Update pragma solidity version to ^0.8.20 in OpenInvariantsTest contract

--- foundry_defi_protocol/test/fuzz/OpenInvariantsTest.t.sol
+++ foundry_defi_protocol/test/fuzz/OpenInvariantsTest.t.sol
@@ -1,5 +1,5 @@
 // // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-// pragma solidity ^0.8.25;
+// pragma solidity ^0.8.20;
 
 // import {Test} from "forge-std/Test.sol";
 // import {StdInvariant} from "forge-std/StdInvariant.sol";
