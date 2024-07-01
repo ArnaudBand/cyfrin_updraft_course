896d905dee54786423a5aed21ed38c8ee841824e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-01 07:21:38 +0000

    refactor: Update DSCEngine to use OracleLib for price feed retrieval

--- foundry_defi_protocol/src/DSCEngine.sol
+++ foundry_defi_protocol/src/DSCEngine.sol
@@ -5,6 +5,7 @@ import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {OracleLib} from "./libraries/OracleLib.sol";
 
 /**
  * @title DSC Engine
@@ -33,6 +34,10 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__MintedFailed();
     error DSCEngine__HealthFactor();
     error DSCEngine__HealthFactorNotImprove();
+
+    // Type
+
+    using OracleLib for AggregatorV3Interface;
 
     // State variables
 
@@ -236,7 +241,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function getTokenAmountFromUsd(address token, uint256 _usdAmountInWei) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_PriceFeeds[token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
         return (_usdAmountInWei * PRECISION) / (uint256(price) * ADDITIONAL_FEED_PRECISION);
     }
 
@@ -322,7 +327,7 @@ contract DSCEngine is ReentrancyGuard {
 
     function _getUsdValue(address token, uint256 _amount) private view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_PriceFeeds[token]);
-        (, int256 price,,,) = priceFeed.latestRoundData();
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
         return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * _amount) / PRECISION;
     }
 
