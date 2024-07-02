e0eb6c426063df55c6b4f1f84e79f8b6eba31987
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-02 04:01:22 +0000

    chore: Update BagelToken contract to use latest version of Solidity

--- foundry_merkle_airdrop/src/BagelToken.sol
+++ foundry_merkle_airdrop/src/BagelToken.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
-contract BagelToken {
-  // Token ERC20
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BagelToken is ERC20, Ownable {
+  constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender){}
+
+  function mint(address to, uint256 amount) external onlyOwner {
+    _mint(to, amount);
+  }
 }
