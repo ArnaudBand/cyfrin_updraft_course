1fc1e2530168fc7a1fadf3f1b72ffcc7ec930e69
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-02 07:51:09 +0000

    chore: Update BagelToken and MerkleAirdrop contracts

--- foundry_merkle_airdrop/src/BagelToken.sol
+++ foundry_merkle_airdrop/src/BagelToken.sol
@@ -5,9 +5,9 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BagelToken is ERC20, Ownable {
-  constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender){}
+    constructor() ERC20("Bagel", "BAGEL") Ownable(msg.sender) {}
 
-  function mint(address to, uint256 amount) external onlyOwner {
-    _mint(to, amount);
-  }
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
 }
--- foundry_merkle_airdrop/src/MerkleAirdrop.sol
+++ foundry_merkle_airdrop/src/MerkleAirdrop.sol
@@ -5,44 +5,50 @@ import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeE
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop {
-  // Some list of addresses
-  // Allow some in the list to claim airdrop
+    // Some list of addresses
+    // Allow some in the list to claim airdrop
 
-  using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-  error MerkleAirdrop__InvalidProof();
-  error MerkleAirdrop__AlreadyClaimed();
+    error MerkleAirdrop__InvalidProof();
+    error MerkleAirdrop__AlreadyClaimed();
 
-  address[] claimers;
-  bytes32 private immutable i_merkleRoot;
-  IERC20 private immutable i_airdropToken;
+    address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
 
-  mapping (address claimer => bool claimed) private s_hasClaimed;
+    mapping(address claimer => bool claimed) private s_hasClaimed;
 
-  event Claim(address indexed account, uint256 amount);
+    event Claim(address indexed account, uint256 amount);
 
-  constructor(bytes32 merkleProofs, IERC20 token) {
-    i_merkleRoot = merkleProofs;
-    i_airdropToken = token;
-  }
-
-  function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-    // Calculate using the account and the amount, the hash -> leaf node
-    bytes32 node = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
-    // Verify the proof
-    if (!MerkleProof.verify(merkleProof, i_merkleRoot, node)) {
-      // If the proof is invalid, revert
-      revert MerkleAirdrop__InvalidProof();
+    constructor(bytes32 merkleProofs, IERC20 token) {
+        i_merkleRoot = merkleProofs;
+        i_airdropToken = token;
     }
-    // Check if the account has already claimed
-    if (s_hasClaimed[account]) {
-      // If the account has already claimed, revert
-      revert MerkleAirdrop__AlreadyClaimed();
-    }
-    // Emit the claim event
-    emit Claim(account, amount);
-    // Transfer the amount to the account
-    i_airdropToken.safeTransfer(account, amount);
 
-  }
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // Check if the account has already claimed
+        if (s_hasClaimed[account]) revert MerkleAirdrop__AlreadyClaimed(); // If the account has already claimed, revert
+
+        // Calculate using the account and the amount, the hash -> leaf node
+        bytes32 node = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+
+        // Verify the proof
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, node)) revert MerkleAirdrop__InvalidProof(); // If the proof is invalid, revert
+
+        // Mark the account as claimed
+        s_hasClaimed[account] = true;
+        // Emit the claim event
+        emit Claim(account, amount);
+        // Transfer the amount to the account
+        i_airdropToken.safeTransfer(account, amount);
+    }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
