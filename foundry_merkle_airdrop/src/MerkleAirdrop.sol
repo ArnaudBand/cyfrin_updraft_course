f387f4b538131259f8372efc0076d8aa8777a235
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-02 04:01:38 +0000

    chore: Update MerkleAirdrop contract to Solidity ^0.8.24 version

--- foundry_merkle_airdrop/src/MerkleAirdrop.sol
+++ foundry_merkle_airdrop/src/MerkleAirdrop.sol
@@ -1,4 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
-contract MerkleAirdrop {}
+contract MerkleAirdrop {
+  // Some list of addresses
+  // Allow some in the list to claim airdrop
+}
