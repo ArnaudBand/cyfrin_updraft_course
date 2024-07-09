37fdc4f965a55e58a36f7e33de4552cbff2fee22
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-09 14:35:21 +0000

    chore: Add MerkleAirdropTest contract for testing

--- foundry_merkle_airdrop/test/MerkleAirdropTest.t.sol
+++ foundry_merkle_airdrop/test/MerkleAirdropTest.t.sol
@@ -1,6 +1,38 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
 
-contract MerkleAirdropTest is Test {}
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    BagelToken public token;
+
+    bytes32 public merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 public proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public proof = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(merkleRoot, token);
+        token.mint(token.owner(), AMOUNT_TO_SEND);
+        token.transfer(address(airdrop), AMOUNT_TO_SEND);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, proof);
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Starting balance: %d", startingBalance);
+        console.log("Ending balance: %d", endingBalance);
+        assertEq(endingBalance, startingBalance + AMOUNT_TO_CLAIM);
+    }
+}
