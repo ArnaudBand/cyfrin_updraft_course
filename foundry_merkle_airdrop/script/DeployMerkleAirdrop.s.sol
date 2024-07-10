419a45ec0a94bd959520e3e276195b966ecd0016
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-10 15:31:44 +0000

    chore: Add DeployMerkleAirdrop script for deploying Merkle Airdrop contract

---
+++ foundry_merkle_airdrop/script/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DeployMerkleAirdrop is Script {
+  BagelToken public token;
+  MerkleAirdrop public airdrop;
+
+  bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+  uint256 private s_amountToTransfer = 4 * 25 * 1e18;
+
+  function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
+    vm.startBroadcast();
+
+    token = new BagelToken();
+    airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
+    token.mint(token.owner(), s_amountToTransfer);
+    token.transfer(address(airdrop), s_amountToTransfer);
+    vm.stopBroadcast();
+    return (airdrop, token);
+  }
+
+  function run() public returns(MerkleAirdrop, BagelToken) {
+
+    return deployMerkleAirdrop();
+  }
+}
