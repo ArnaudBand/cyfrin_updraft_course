e27087550bd77ecae66d0a7ef7f5311b2504f29d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-13 04:16:54 +0000

    chore: Refactor DeployMerkleAirdrop script for readability and consistency

--- foundry_merkle_airdrop/script/DeployMerkleAirdrop.s.sol
+++ foundry_merkle_airdrop/script/DeployMerkleAirdrop.s.sol
@@ -7,25 +7,24 @@ import {BagelToken} from "../src/BagelToken.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployMerkleAirdrop is Script {
-  BagelToken public token;
-  MerkleAirdrop public airdrop;
+    BagelToken public token;
+    MerkleAirdrop public airdrop;
 
-  bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
-  uint256 private s_amountToTransfer = 4 * 25 * 1e18;
+    bytes32 private s_merkleRoot = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private s_amountToTransfer = 4 * 25 * 1e18;
 
-  function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
-    vm.startBroadcast();
+    function deployMerkleAirdrop() public returns (MerkleAirdrop, BagelToken) {
+        vm.startBroadcast();
 
-    token = new BagelToken();
-    airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
-    token.mint(token.owner(), s_amountToTransfer);
-    token.transfer(address(airdrop), s_amountToTransfer);
-    vm.stopBroadcast();
-    return (airdrop, token);
-  }
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
+        token.mint(token.owner(), s_amountToTransfer);
+        token.transfer(address(airdrop), s_amountToTransfer);
+        vm.stopBroadcast();
+        return (airdrop, token);
+    }
 
-  function run() public returns(MerkleAirdrop, BagelToken) {
-
-    return deployMerkleAirdrop();
-  }
+    function run() public returns (MerkleAirdrop, BagelToken) {
+        return deployMerkleAirdrop();
+    }
 }
