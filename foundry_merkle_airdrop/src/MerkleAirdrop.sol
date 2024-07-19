db923cf1c00fdd1021aeb5b1ff35e838b2b7b445
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-19 07:18:43 +0000

    chore: Add ECDSA library for signature verification in MerkleAirdrop contract

--- foundry_merkle_airdrop/src/MerkleAirdrop.sol
+++ foundry_merkle_airdrop/src/MerkleAirdrop.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract MerkleAirdrop is EIP712 {
     // Some list of addresses
@@ -40,7 +41,7 @@ contract MerkleAirdrop is EIP712 {
         if (s_hasClaimed[account]) revert MerkleAirdrop__AlreadyClaimed(); // If the account has already claimed, revert
 
         // check if the signature is valid
-        if (!_isValidSignture(account, amount, getMessage(account, amount), v, r, s)) revert MerkleAirdrop__InvalidSignature(); // If the signature is invalid, revert
+        if (!_isValidSignture(account, getMessage(account, amount), v, r, s)) revert MerkleAirdrop__InvalidSignature(); // If the signature is invalid, revert
 
         // Calculate using the account and the amount, the hash -> leaf node
         bytes32 node = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
@@ -68,5 +69,10 @@ contract MerkleAirdrop is EIP712 {
 
     function getAirdropToken() external view returns (IERC20) {
         return i_airdropToken;
+    }
+
+    function _isValidSignture(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s) internal pure returns (bool) {
+        (address actualSigner, , ) = ECDSA.tryRecover(digest, v, r, s);
+        return actualSigner == account;
     }
 }
