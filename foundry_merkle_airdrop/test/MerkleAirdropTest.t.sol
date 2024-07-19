207648c733438a2f7f299013593ee59a6a9b125b
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-19 07:32:43 +0000

    chore: Update MerkleAirdropTest to include gasPayer and sign user's claim message

--- foundry_merkle_airdrop/test/MerkleAirdropTest.t.sol
+++ foundry_merkle_airdrop/test/MerkleAirdropTest.t.sol
@@ -17,6 +17,7 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     bytes32 public proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 public proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] public proof = [proofOne, proofTwo];
+    address gasPayer;
     address user;
     uint256 userPrivKey;
 
@@ -32,11 +33,15 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
         }
 
         (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
 
     function testUsersCanClaim() public {
-        vm.prank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, proof);
+        bytes32 digest = airdrop.getMessage(user, AMOUNT_TO_CLAIM);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, proof, v, r, s);
         uint256 endingBalance = token.balanceOf(user);
         console.log("Ending balance: %d", endingBalance);
         assertEq(endingBalance, AMOUNT_TO_CLAIM);
