6c373ea6b7a4fbc7d7198d09417f24e94fa7d4e2
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-07-26 09:25:42 +0000

    chore: Add Interact.s.sol script for interacting with Merkle Airdrop contract

---
+++ foundry_merkle_airdrop/script/Interact.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+
+contract ClaimAirdrop is Script {
+
+}
