9a403abb258b994f89643f8f966708289edd3106
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-08-06 19:14:36 +0000

    chore: Update openzeppelin-contracts submodule to latest commit

--- foundry_upgradable/src/BoxV1.sol
+++ foundry_upgradable/src/BoxV1.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable {
     uint256 private value;
+
+    constructor() {
+        _disableInitializers();
+    }
 
     // Gets the current value
     function getNumber() external view returns (uint256) {
@@ -12,5 +20,9 @@ contract BoxV1 {
     // Reads the last stored value
     function version() public pure returns (uint256) {
         return 1;
+    }
+
+    function _authorizeUpgrade(address) internal override {
+        // This function is empty, but it is required to be implemented
     }
 }
