4ddc361c59e02ce429c36e1418b17da293ee3b3e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-08-16 21:19:39 +0000

    chore: Update openzeppelin-contracts submodule to commit dbb6104

--- foundry__account_abstraction/src/ethereum/MinimalAccount.sol
+++ foundry__account_abstraction/src/ethereum/MinimalAccount.sol
@@ -3,13 +3,27 @@ pragma solidity ^0.8.24;
 
 import {IAccount} from "account-abstraction/interfaces/IAccount.sol";
 import {PackedUserOperation} from "account-abstraction/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "account-abstraction/core/Helpers.sol";
 
-contract MinimalAccount is IAccount {
-      function validateUserOp(
-        PackedUserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256 missingAccountFunds
-    ) external returns (uint256 validationData) {
-      
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+       validationData = _validateSignature(userOp, userOpHash);
+    }
+
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash) internal view returns (uint256 validatiionData) {
+      bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+      address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+      if (signer != owner()) {
+        return SIG_VALIDATION_FAILED;
+      }
+      return SIG_VALIDATION_SUCCESS;
     }
 }
