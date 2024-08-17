9f3998cc488669e1b11a12fcf070833333c4d6c7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-08-17 05:53:18 +0000

    feat: Add DeployMinimalAccount script for deploying MinimalAccount contract

---
+++ foundry__account_abstraction/script/DeployMinimalAccount.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+contract DeployMinimalAccount is Script {
+    function run() public {}
+
+    function deployMinimalAccount() public {
+        HelperConfig helperConfig = new HelperConfig();
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        vm.startBroadcast();
+        MinimalAccount minimalAccount = new MinimalAccount(config.entryPoint);
+        minimalAccount.transferOwnership(msg.sender);
+        vm.stopBroadcast();
+
+    }
+}
