f4d91fd27f525d3b362bcbf9c8af53aee1e2ad36
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-08-29 02:20:25 +0000

    refactor: Update ZksynMinimalAccount contract with transaction lifecycle comments

--- foundry__account_abstraction/src/zkSync/ZksynMinimalAccount.sol
+++ foundry__account_abstraction/src/zkSync/ZksynMinimalAccount.sol
@@ -4,6 +4,24 @@ pragma solidity 0.8.24;
 import {IAccount} from "foundry-era-contracts/contracts/interfaces/IAccount.sol";
 import {Transaction} from "foundry-era-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 
+/**
+ * Lifecycle of a type 113 (0x71) transaction
+ * msg.sender is the bootloader system contract
+ *
+ * Phase 1 Validation
+ * 1. The user sends the transaction to the "zkSync API client" (sort of a "light node")
+ * 2. The zkSync API client checks to see the the nonce is unique by querying the NonceHolder system contract
+ * 3. The zkSync API client calls validateTransaction, which MUST update the nonce
+ * 4. The zkSync API client checks the nonce is updated
+ * 5. The zkSync API client calls payForTransaction, or prepareForPaymaster & validateAndPayForPaymasterTransaction
+ * 6. The zkSync API client verifies that the bootloader gets paid
+ *
+ * Phase 2 Execution
+ * 7. The zkSync API client passes the validated transaction to the main node / sequencer (as of today, they are the same)
+ * 8. The main node calls executeTransaction
+ * 9. If a paymaster was used, the postTransaction is called
+ */
+
 contract ZksyncMinimalAccount is IAccount {
     function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
         external
