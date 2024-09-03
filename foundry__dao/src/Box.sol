225af5e9d26ec20596b910e5bfd2f68a15d41815
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-03 07:52:12 +0300

    feat: Add Box contract for storing numbers

--- foundry__dao/src/Box.sol
+++ foundry__dao/src/Box.sol
@@ -4,18 +4,18 @@ pragma solidity 0.8.24;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Box is Ownable {
-  uint256 private s_number;
+    uint256 private s_number;
 
-  event NumberChanged(uint256 number);
+    event NumberChanged(uint256 number);
 
-  constructor() Ownable(msg.sender) {}
+    constructor() Ownable(msg.sender) {}
 
-  function store(uint256 number) public onlyOwner {
-    s_number = number;
-    emit NumberChanged(number);
-  }
+    function store(uint256 number) public onlyOwner {
+        s_number = number;
+        emit NumberChanged(number);
+    }
 
-  function getNumber() external view returns (uint256) {
-    return s_number;
-  }
+    function getNumber() external view returns (uint256) {
+        return s_number;
+    }
 }
