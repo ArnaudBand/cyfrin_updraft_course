672d62042f3315b3aeafcca6b8d4de6d95fb6f29
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-03 18:23:41 +0300

    feat: Add test for updating Box without governance

--- foundry__dao/test/MyGovernorTest.t.sol
+++ foundry__dao/test/MyGovernorTest.t.sol
@@ -8,45 +8,45 @@ import {TimeLock} from "../src/TimeLock.sol";
 import {Box} from "../src/Box.sol";
 
 contract MyGovernorTest is Test {
+    // contracts
+    MyGovernor governor;
+    GovernToken token;
+    TimeLock timelock;
+    Box box;
 
-  // contracts
-  MyGovernor governor;
-  GovernToken token;
-  TimeLock timelock;
-  Box box;
+    address public USER = makeAddr("user");
+    uint256 public constant INITIAL_SUPPLY = 100 ether;
 
-  address public USER = makeAddr("user");
-  uint256 public constant INITIAL_SUPPLY = 100 ether;
+    address[] public proposers;
+    address[] public executors;
+    uint256[] public values;
+    bytes[] public calldatas;
+    address[] public targets;
 
-  address[] public proposers;
-  address[] public executors;
-  uint256[] public values;
-  bytes[] public calldatas;
-  address[] public targets;
+    uint256 public constant MINI_DELAY = 3600; // 1 hour - after proposal is approved
+    uint256 public constant VOTING_DELAY = 1; // 1 block
 
-  uint256 public constant MINI_DELAY = 3600; // 1 hour - after proposal is approved
+    function setUp() public {
+        token = new GovernToken();
+        token.mint(USER, INITIAL_SUPPLY);
 
-  function setUp() public {
-    token = new GovernToken();
-    token.mint(USER, INITIAL_SUPPLY);
+        vm.startBroadcast(USER);
+        token.delegate(USER);
+        timelock = new TimeLock(MINI_DELAY, proposers, executors);
+        governor = new MyGovernor(token, timelock);
 
-    vm.startBroadcast(USER);
-    token.delegate(USER);
-    timelock = new TimeLock(MINI_DELAY, proposers, executors);
-    governor = new MyGovernor(token, timelock);
+        bytes32 proposerRole = timelock.PROPOSER_ROLE();
+        bytes32 executorRole = timelock.EXECUTOR_ROLE();
+        bytes32 adminRole = timelock.DEFAULT_ADMIN_ROLE();
 
-    bytes32 proposerRole = timelock.PROPOSER_ROLE();
-    bytes32 executorRole = timelock.EXECUTOR_ROLE();
-    bytes32 adminRole = timelock.DEFAULT_ADMIN_ROLE();
+        timelock.grantRole(proposerRole, address(governor));
+        timelock.grantRole(executorRole, address(governor));
+        timelock.revokeRole(adminRole, USER);
+        vm.stopBroadcast();
 
-    timelock.grantRole(proposerRole, address(governor));
-    timelock.grantRole(executorRole, address(governor));
-    timelock.revokeRole(adminRole, USER);
-    vm.stopBroadcast();
-
-    box = new Box();
-    box.transferOwnership(address(timelock));
-  }
+        box = new Box();
+        box.transferOwnership(address(timelock));
+    }
 
     function testCanUpdateBoxWithoutGovernace() public {
         vm.expectRevert();
@@ -54,15 +54,32 @@ contract MyGovernorTest is Test {
     }
 
     function testGovernanceUpdateProposalState() public {
-      string memory description = "Update Box";
-      bytes memory data = abi.encodeWithSignature("store(uint256)", 42);
+        string memory description = "Update Box";
+        bytes memory data = abi.encodeWithSignature("store(uint256)", 42);
 
-      values.push(0);
-      calldatas.push(data);
-      targets.push(address(box));
+        values.push(0);
+        calldatas.push(data);
+        targets.push(address(box));
 
-      // 1. Propose to the DAO
-      uint256 proposalId = governor.propose(targets, values, calldatas, description);
-      assertEq(uint256(governor.state(proposalId)), 0);
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+        assertEq(uint256(governor.state(proposalId)), 0);
+    }
+
+    function testGovernanceCanBeActiveAfterVotingDelay() public {
+        string memory description = "Update Box";
+        bytes memory data = abi.encodeWithSignature("store(uint256)", 42);
+
+        values.push(0);
+        calldatas.push(data);
+        targets.push(address(box));
+
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(targets, values, calldatas, description);
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        assertEq(uint256(governor.state(proposalId)), 1);
     }
 }
