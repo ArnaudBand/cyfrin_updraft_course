3f569086cc6c44f74113028189e0258795573b16
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-09-03 18:23:29 +0300

    refactor: Improve code readability and consistency by renaming GovToken contract to GovernToken

--- foundry__dao/src/TimeLock.sol
+++ foundry__dao/src/TimeLock.sol
@@ -4,13 +4,12 @@ pragma solidity 0.8.24;
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
-
-  /**
-   * 
-   * @notice miniDelay The minimum delay for the timelock
-   * @notice proposers The list of proposers
-   * @notice executors The list of executors
-   */
+    /**
+     *
+     * @notice miniDelay The minimum delay for the timelock
+     * @notice proposers The list of proposers
+     * @notice executors The list of executors
+     */
     constructor(uint256 miniDelay, address[] memory proposers, address[] memory executors)
         TimelockController(miniDelay, proposers, executors, msg.sender)
     {}
